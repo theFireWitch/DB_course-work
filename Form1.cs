58cf86580c0ed709aee92db6b4c91c9a53a58502
Witch <pyatigorets.tanya@lll.kpi.ua>
2024-12-20 18:58:21 +0200

    first

---
+++ Form1.cs
@@ -0,0 +1,20 @@
+namespace Курсова_робота
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.LightBlue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.LightGreen;
+        }
+    }
+}
